fe3991a2051915af0f61142bca5afb8ac7d1b215
Borisovna <hipsavita@gmail.com>
2023-02-22 20:28:17 +0300

    end

---
+++ task1/task1.cs
@@ -0,0 +1,26 @@
+﻿// // Задача 1
+// // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Проверка без применения строк
+// // 14212 -> нет
+// // 12821 -> да
+// // 23432 -> да
+
+ Console.WriteLine ("Введите 5-ти значное число-> ");
+ string number = Console.ReadLine();
+ int pal = number.Length;
+
+ if (pal == 5)
+ {
+     if (number[0] == number[4] && number [1] == number[3])
+    {
+        Console.WriteLine($" Число {number}  палиндром");
+    }
+
+     else
+     {
+        Console.WriteLine($" Число {number} не палиндром");
+    }
+}
+ else
+ {
+    Console.WriteLine ("число не подходит, нужно пятизначное!");
+}
---
+++ task2/task2.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = ReadInt("Введите х1 : ");
+int y1 = ReadInt("Введите y1 : ");
+int z1 = ReadInt("Введите z1 : ");
+int x2 = ReadInt("Введите х2: ");
+int y2 = ReadInt("Введите y2: ");
+int z2 = ReadInt("Введите z2: ");
+
+int Xi = x2 - x1;
+int Yi = y2 - y1;
+int Zi = z2 - z1;
+
+double length = Math.Sqrt(Xi * Xi + Yi * Yi + Zi * Zi);
+Console.WriteLine($"Расстояние между точками равно в 3D пространстве равно  {length} ");
+
+
